3d1a7cba5aafb9d049889b919b38cfc4f0a54c9a
vinay9009patel <vinay9009patel@gmail.com>
2026-03-27 23:22:27 +0530

    Day4. Add Height of a Binary Tree implementation

---
+++ hackerRank  /LinkedList/Tree/HeightofaBinaryTree.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static int height(Node root) {
+      	// Write your code here.
+       
+    // Base case: Null node ki height 0
+    if (root == null) {
+        return -1;
+    }
+    
+    // Left subtree ki height
+    int leftHeight = height(root.left);
+    
+    // Right subtree ki height
+    int rightHeight = height(root.right);
+    
+    // Current node ki height = max(left, right) + 1
+    return Math.max(leftHeight, rightHeight) + 1;
+}
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }	
+}
