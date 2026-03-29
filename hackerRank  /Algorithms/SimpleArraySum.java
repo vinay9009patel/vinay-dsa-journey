9333062c62738f0f4ae8533cc6c756a850a8e8e7
vinay9009patel <vinay9009patel@gmail.com>
2026-03-29 13:57:20 +0800

    Day4. Update SimpleArraySum.java

--- hackerRank  /Algorithms/SimpleArraySum.java
+++ hackerRank  /Algorithms/SimpleArraySum.java
@@ -21,7 +21,13 @@ class Result {
 
     public static int simpleArraySum(List<Integer> ar) {
     // Write your code here
+int result =0;
+for(int i= 0;i<ar.size() ;i++){
+  result +=  ar.get(i);
+}
 
+
+return result ;
     }
 
 }
