57d1751010f882eca4f414868b8ce7c6e218e9c6
vinay9009patel <vinay9009patel@gmail.com>
2026-03-29 13:58:17 +0800

    Day4. Update SolveMeFirst.java ,easy question

--- hackerRank  /Algorithms/SolveMeFirst.java
+++ hackerRank  /Algorithms/SolveMeFirst.java
@@ -8,8 +8,8 @@ public class Solution {
 
 
     static int solveMeFirst(int a, int b) {
-      	// Hint: Type return a+b; below 
-
+     
+return a+b;
    }
 
  public static void main(String[] args) {
@@ -23,3 +23,4 @@ public class Solution {
         System.out.println(sum);
    }
 }
+
