d2e64afb87f62a024a91259b16861355304f1c31
vinay9009patel <vinay9009patel@gmail.com>
2026-04-11 19:59:16 +0800

    Day14. Implement inorder traversal for binary tree

---
+++ LeetCode/Tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+        dsf(list, root);
+        return list;
+    }
+
+    void dsf(List<Integer> list, TreeNode root) {
+
+        if (root == null)
+            return;
+        dsf(list, root.left);
+
+        list.add(root.val);
+        dsf(list, root.right);
+
+    }
+
+}
