17fff68a28653b7df618289ae039fb31c8e6a01a
vinay9009patel <vinay9009patel@gmail.com>
2026-04-14 16:05:18 +0800

    Day14.Implement copy of linked list with random pointers

---
+++ LeetCode/CopyListwithRandomPointer.java
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Node curr = head;
+
+        //  Step 1: insert copy nodes
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        //  Step 2: set random pointers
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        //  Step 3: separate lists
+        curr = head;
+        Node newHead = head.next;
+
+        while (curr != null) {
+            Node copy = curr.next;
+
+            curr.next = copy.next;
+
+            if (copy.next != null) {
+                copy.next = copy.next.next;
+            }
+
+            curr = curr.next;
+        }
+
+        return newHead;
+    }
+}
