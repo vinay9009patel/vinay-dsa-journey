903cec172b8a550f6d6511ab41a459ae6dc628e0
vinay9009patel <vinay9009patel@gmail.com>
2026-04-24 20:29:56 +0800

    Day15. Add solution for unique number of occurrences

---
+++ LeetCode/HashMap/Unique Number of Occurrences.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+     HashMap <Integer, Integer> map = new HashMap<>();
+
+     for(int num  : arr){
+      if(!map.containsKey(num)){
+      map.put(num,1);
+      }else {
+        map.put(num, map.get(num) +1);
+      }
+     } 
+     HashSet <Integer> set = new HashSet<>();
+     for(int loop : map.values()){
+         if(set.contains(loop) ) return false;
+         set.add(loop);   
+     
+     
+     }  
+     return true ; 
+    }
+}
