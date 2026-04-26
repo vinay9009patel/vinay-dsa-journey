535e540dca452d445264162e90f2070ad07d9f77
vinay9009patel <vinay9009patel@gmail.com>
2026-04-26 10:26:50 +0800

    Day16. Implement reverseWords method to reverse string words

---
+++ LeetCode/String/Reverse Words in a String.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        int i = s.length() - 1;
+
+        while (i >= 0) {
+
+       
+            while (i >= 0 && s.charAt(i) == ' ') i--;
+
+            if (i < 0) break;
+
+            int j = i;
+
+         
+            while (i >= 0 && s.charAt(i) != ' ') i--;
+ 
+            sb.append(s.substring(i + 1, j + 1));
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
