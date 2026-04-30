1787a5792da16a3222bd2ccc72fc4504576e36e0
vinay9009patel <vinay9009patel@gmail.com>
2026-04-30 12:15:07 +0530

    Day18. Add solution for finding repeated DNA sequences ,using hashSet and sliding window

---
+++ LeetCode/Array/Repeated DNA Sequences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seen = new HashSet<>();
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < s.length() - 10; i++) {
+            String sub = s.substring(i, i + 10);
+            if (seen.contains(sub)) {
+                result.add(sub);
+            } else {
+                seen.add(sub);
+
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
