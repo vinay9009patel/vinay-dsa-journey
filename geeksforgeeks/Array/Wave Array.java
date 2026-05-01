61f156441953cea471eaa61742a423244d39a8e1
vinay9009patel <vinay9009patel@gmail.com>
2026-05-02 01:08:03 +0530

    Day20.Implement sortInWave method in Wave Array class

---
+++ geeksforgeeks/Array/Wave Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void sortInWave(int arr[]) {
+        // code here
+        
+     
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
