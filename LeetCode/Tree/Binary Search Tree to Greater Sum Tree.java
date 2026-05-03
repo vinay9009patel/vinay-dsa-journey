bba8aa003cb4b70415d66e6341dbeaa95e3de9f6
vinay9009patel <vinay9009patel@gmail.com>
2026-05-04 05:57:07 +0800

    Day22.Add BST to Greater Sum Tree conversion
    Implement conversion of a Binary Search Tree to a Greater Sum Tree where each node's value is replaced with the sum of all values greater than or equal to it.

---
+++ LeetCode/Tree/Binary Search Tree to Greater Sum Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        reverse(root);
+        return root;
+    }
+
+    private void reverse(TreeNode node) {
+        if (node == null) return;
+
+        reverse(node.right);
+
+        sum += node.val;
+        node.val = sum;
+
+        reverse(node.left);
+    }
+}
