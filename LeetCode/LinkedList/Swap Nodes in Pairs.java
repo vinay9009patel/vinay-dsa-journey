5e80a8104e29e854ec5d38197fc68b5cb12a5235
vinay9009patel <vinay9009patel@gmail.com>
2026-05-15 12:11:49 +0800

    Day27. Add swapPairs method for linked list node swapping
    Implement a solution to swap adjacent nodes in pairs in a singly-linked list.

---
+++ LeetCode/LinkedList/Swap Nodes in Pairs.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        int k=2;
+          ListNode temp =head;
+        int count =0; 
+        while (temp !=null &&count <k){
+           temp = temp.next;
+            count ++;
+        }
+   if(count <k){
+    return head;
+   }
+
+
+    ListNode prev=null , curr =head,next =null;
+   
+     count=0;
+    while(curr!=null&&count<k){
+        next =curr.next;
+        curr.next =prev;
+        prev=curr;
+        curr=next;
+        count++;
+    }
+
+
+head.next= swapPairs(curr) ;
+
+return prev;
+
+    }
+}
