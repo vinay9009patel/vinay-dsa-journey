fe5f153c1c88dd5b3b31340f4aa520975de297c7
vinay9009patel <vinay9009patel@gmail.com>
2026-09-07 00:07:37 +0530

    Add files via upload

---
+++ Codeforces/900/HQ9plus.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class HQ9plus {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        if (s.contains("H") || 
+            s.contains("Q") || 
+            s.contains("9")) {
+
+            System.out.println("YES");
+
+        } else {
+
+            System.out.println("NO");
+        }
+    }
+}
